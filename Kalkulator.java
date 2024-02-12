daf97bc07d5c142686c6ee99cd8bfca2b2cc594b
Hammam23 <158988207+Hammam23@users.noreply.github.com>
2024-02-12 10:34:32 +0700

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -16,7 +16,7 @@ public class Kalkulator {
         System.out.println("5. Hasil bagi");
 
 
-        System.out.println("Masukkan Pilihan (1/2/3/4/5) :");
+        System.out.println("Masukkan Pilihan (1/2/3/4/6) :");
 
         String pilihan = scanner.nextLine();
         if (pilihan.equals("1") || pilihan.equals("2") || pilihan.equals("3") || pilihan.equals("4")){
